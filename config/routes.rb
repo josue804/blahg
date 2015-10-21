8cb77b889c14b1be1392539a8035491c72035233
Josue Valverde <josue@gmail.com>
2015-10-20 23:02:13 -0700

    add routing for nested resources.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
 
-  resources :posts
+  resources :posts do
+    resources :tags
+  end
+
+  resources :tags do
+    resources :posts
+  end
 end
